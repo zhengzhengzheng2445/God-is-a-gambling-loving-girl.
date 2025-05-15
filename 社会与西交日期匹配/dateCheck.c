66b63d7e11f386a7639a69042fe2dd032c3f4b45
骰子与猫尾定律 <13846053645@163.com>
2025-05-16 01:54:13 +0800

    Add files via upload

--- 社会与西交日期匹配/dateCheck.c
+++ 社会与西交日期匹配/dateCheck.c
@@ -6,11 +6,12 @@ int dateCheck() {
 	time_t ti;
 	time(&ti);
 	struct tm* datep;
-	datep = gmtime(&ti);
-	if (datep->tm_mon == 8 && datep->tm_mday == 24) {
+	datep = localtime(&ti);//年加1900,月加1
+	//printf("%d %d %d", datep->tm_year+1900, datep->tm_mon+1,datep->tm_mday);
+	if ((datep->tm_mon + 1) == 8 && datep->tm_mday == 24) {
 		return 2;
 	}
-	else if (datep->tm_mon == 2 && datep->tm_mday == 3) {
+	else if ((datep->tm_mon + 1) == 2 && datep->tm_mday == 3) {
 		return 1;
 	}
 	else {
