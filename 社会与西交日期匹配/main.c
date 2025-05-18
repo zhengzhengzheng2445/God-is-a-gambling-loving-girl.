da3802928dd6b1aaa3cb20e7e652854990b971c3
骰子与猫尾定律 <13846053645@163.com>
2025-05-19 02:19:18 +0800

    Add files via upload

--- 社会与西交日期匹配/main.c
+++ 社会与西交日期匹配/main.c
@@ -1,25 +1,27 @@
 #define _CRT_SECURE_NO_WARNINGS
-#define no 135644
+#define NO 135644
 #include <stdio.h>
+#include "test.h"
 int main() {
 	while(1)
 	{
-		int remindNmb = no, selectFunction = no;                                               //提醒次数,功能选择
-		int tmRecord[8] = {no,no,no,no,no,no,no,no};
-		remind(timeGet(tmRecord));
-		printf("请选择功能:\n1.转换日期\n2.查看图片\n3.更新修改数据\n0.退出程序");
+		int remindNmb = NO, selectFunction = NO;                                               //提醒次数,功能选择
+		int tmRecord[8] = {NO,NO,NO,NO,NO,NO,NO,NO};
+		timeGet(tmRecord);
+		remind(tmRecord);
+		printf("请选择功能:\n1.转换日期\n2.查看图片\n3.更新修改数据\n0.退出程序\n");
 		selectFunction = defenceMistake(selectFunction, 0, 3);                                 //验证输入是否有效
-		if (selectFunction = 1) {
+		if (selectFunction == 1) {
 			datetrans();
 		}
-		else if (selectFunction = 2) {
+		else if (selectFunction == 2) {
 			photoView();
 		}
-		else if(selectFunction = 2){
+		else if(selectFunction == 3){
 			updateData();
 		}
 		else {
-			break;
+		  break;
 		}
 	}
 	return 0;
